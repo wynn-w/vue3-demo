7a29a3ed657d5d9db177b5baa3b3400448dccc36
wynn-w <wwwynn22@gmail.com>
2021-12-22 00:39:14 +0800

    feat: ref 解包详细探讨

--- src/view/refApi/index.vue
+++ src/view/refApi/index.vue
@@ -4,16 +4,25 @@
     <button @click="add">press</button>
     <div>
       <!-- 在 template 中自动结构 num.value 并将值赋值给 num -->
-      <span>num: </span>{{num}}
-      <br>
+      <div>num: {{num}}</div>
       <!-- num.value 直接取不到值了，改了吗？ -->
-      <span>num.value: </span>{{num.value}}
+      <div>num.value: {{num.value}}</div>
+      <!-- 经测试发现以下三种情况都可 ref 解包 -->
+      <!-- 与传闻中的 ref 只能浅层解包不相符 -->
+      <div>baseNum.num: {{baseNum.num}}</div>
+      <div>baseNum.num.value: {{baseNum.num.value}}</div>
+      <div>baseNum: {{baseNum}}</div>
+      
+      <div>reactNum.num: {{reactNum.num}}</div>
+      <div>reactNum.num.value: {{reactNum.num.value}}</div>
+      <div>reactNum: {{reactNum}}</div>
+
     </div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue' // volar 里面居然没有自动导入。。
+import { reactive, ref } from 'vue' // volar 里面居然没有自动导入。。
 export default {
   name: 'RefApi',
   props: {
@@ -21,6 +30,12 @@ export default {
   },
   setup() {
     const num = ref(0)
+    const baseNum = {
+      num
+    }
+    const reactNum = reactive({
+      num
+    })
     // 而在 Javascript 中需要手动调用 .value 属性进行值操作
     const add = () => {
       console.log(num.value);
@@ -28,7 +43,9 @@ export default {
     }
     return {
       num,
-      add,
+      baseNum,
+      reactNum,
+      add
     }
   },
   // setup(props, content) {
