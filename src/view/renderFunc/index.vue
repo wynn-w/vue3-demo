561db4da3308cbc124464625ea915c60bfebfd42
wynn-w <wwwynn22@gmail.com>
2022-01-11 01:14:05 +0800

    feat: 基于 setup 函数 + setup 内部 render 函数实现「默认/具名/作用域插槽」的实现

--- src/view/renderFunc/index.vue
+++ src/view/renderFunc/index.vue
@@ -1,34 +1,62 @@
 <script>
+import mSlot from './mSlot.vue';
 import { h, ref } from 'vue';
 export default {
     setup() {
-        let num = ref(0);
+        let num = ref(1);
         return () => {
             return h(
-            'div',
-            {
-                class: 'm-render-container'
-            },
-            [
-                h('p', {class:'show-value'}, num.value),
-                h(
-                    'button', 
+                'div', 
+                {
+                    class: 'lala'
+                }, 
+                [
+                    h('p', null, 'dadada...'),
+                    h(mSlot, {}, `num: ${num.value}`), // 从视图展示结果来看，任意值进去都走 default 插槽
+                    h(mSlot, {}, 
                     {
-                        onclick: ()=> num.value++ // 没有这样的简写 onclick(){...}
-                    }, 
-                    '+'
-                ),
-                h(
-                    'button', 
-                    {
-                        onclick: ()=> num.value = 0 // 没有这样的简写 onclick(){...}
-                    }, 
-                    'reset'
-                )
-            ]
-        )
+                        default: () => h('span', null, `default slot change`),
+                        setName: () => h('span', null, `setName slot change`), // 具名插槽
+                        sendProps: (props) => h('span', null, `sendProps slot: ${props.msg}`), // 作用域插槽
+                    }
+                    ), 
+
+                    h('p', null, 'dadada...')
+                ]
+            );
         }
-    },
+    }
+
+    // ==================================================== setup render --base
+    // setup() {
+    //     let num = ref(0);
+    //     return () => {
+    //         return h(
+    //         'div',
+    //         {
+    //             class: 'm-render-container'
+    //         },
+    //         [
+    //             h('p', {class:'show-value'}, num.value),
+    //             h(
+    //                 'button', 
+    //                 {
+    //                     onclick: ()=> num.value++ // 没有这样的简写 onclick(){...}
+    //                 }, 
+    //                 '+' 
+    //             ),
+    //             h(
+    //                 'button', 
+    //                 {
+    //                     onclick: ()=> num.value = 0 // 没有这样的简写 onclick(){...}
+    //                 }, 
+    //                 'reset'
+    //             )
+    //         ]
+    //     )
+    //     }
+    // },
+    // =================================================== render function
     // render() {
     //     // return h('div', {}, 'sss') // 基础使用 第二个参数为 html 属性/方法，第三个参数为内容（innerHTML）
     //     return h(
---
+++ src/view/renderFunc/mSlot.vue
@@ -0,0 +1,33 @@
+<script>
+import { h } from 'vue';
+export default {
+    setup(props, {slots}) {
+        return () => {
+            return h(
+                'div', 
+                null, 
+                [
+                    h('p', null, 'there has a slot 👇'),
+                    h(
+                        'div', 
+                        null,
+                        slots.default ? slots.default() : h('span', null, 'default slot placehold'), 
+                    ),
+                    // null,
+                    // `${slots}`
+                    h(
+                        'div', 
+                        null,
+                        slots.setName ? slots.setName() : h('span', null, 'setName slot placehold')
+                    ),
+                    h(
+                        'div', 
+                        null,
+                        slots.sendProps ? slots.sendProps({msg: 'slot msg'}) : h('span', null, '')
+                    )
+                ]
+            )
+        }
+    }
+}
+</script>
