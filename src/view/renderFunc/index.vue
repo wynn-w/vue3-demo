2f309a208ea506f1ddcf85c8e778c3c753e8b455
wynn-w <wwwynn22@gmail.com>
2022-01-11 00:14:30 +0800

    feat:  基于 render 函数编写函数式组件

---
+++ src/view/renderFunc/index.vue
@@ -0,0 +1,36 @@
+<script>
+import { h, ref } from 'vue';
+export default {
+    setup() {
+        let num = ref(0);
+        return {
+            num
+        }
+    },
+    render() {
+        // return h('div', {}, 'sss') // 基础使用 第二个参数为 html 属性/方法，第三个参数为内容（innerHTML）
+        return h(
+            'div',
+            {
+                class: 'm-render-container'
+            },
+            [
+                h('p', {class:'show-value'}, this.num),
+                h(
+                    'button', 
+                    {
+                        onclick: ()=> this.num++ // 没有这样的简写 onclick(){...}
+                    }, 
+                    '+'
+                ),
+                h(
+                    'button', 
+                    {
+                        onclick: ()=> this.num = 0 // 没有这样的简写 onclick(){...}
+                    }, 
+                    'reset'
+                )
+            ])
+    },
+}
+</script>
