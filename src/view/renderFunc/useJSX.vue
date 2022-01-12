e1f2649c69bfd1193a450eb4b38fc06c6be2a69c
wynn-w <wwwynn22@gmail.com>
2022-01-13 01:04:16 +0800

    feat: 完成在 JSX 语法中同时调用「逻辑层」与「视图层」

--- src/view/renderFunc/useJSX.vue
+++ src/view/renderFunc/useJSX.vue
@@ -1,15 +1,26 @@
 <script>
-// import { render } from 'vue';
+import { reactive, ref } from 'vue';
 export default {
     setup() {
+        const aa = ref('xiaoming');
+        const arr = reactive([1,2,4]);
         return {
-            render(){
-                return (
-                    <div>JSX code</div>
-                )
-            }
+            aa,
+            arr
         }
     },
-    
+    render(){
+        return (
+            <div>
+                <h1 style="margin: 0 auto">JSX code</h1>
+                <p>aa: {this.aa}</p>
+                <p>use Javascript generate HTMLElement</p>
+                {
+                    this.arr.map(item => (<p>{item}</p>))
+                }
+            </div>
+
+        )
+    }
 }
 </script>
